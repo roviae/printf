856f476d3d52b8f5d0f08872e59dfb85b4a5069b
munenecarl <munenecarl@gmail.com>
2023-03-26 15:31:40 +0300

    addition of cfiles

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+struct global
+{
+	char *format;
+	int i;
+	int j;
+	int count;
+	va_list args;
+	char *str;
+} id;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+void _printint(void);
+void _printchar(void);
+void _printstring(void);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,106 @@
+#include "main.h"
+
+/**
+ * printchar - prints characters
+ * Return: void
+*/
+void _printchar(void)
+{
+	_putchar(va_arg(id.args, int));
+	id.count++;
+}
+
+/**
+ * printstr - prints strings
+ * Return: void
+*/
+void _printstr(void)
+{
+	id.str = va_arg(id.args, char *);
+	if (id.str == NULL)
+		id.str = "(null)";
+	else
+	{
+		id.j = 0;
+		while (id.str[id.j] != '\0')
+		{
+			_putchar(id.str[id.j]);
+			id.j++;
+			id.count++;
+		}		
+	}	
+}
+
+/**
+ * printint - prints int
+ * Return: void
+*/
+void _printint(void)
+{
+	int num, div, length;
+	
+	num = va_arg(id.args, int);
+	div = 1;
+	length = 0;
+	if (num < 0)
+	{
+		_putchar('_');
+		num = num * -1;
+		id.count++;
+	}
+	while (num / div > 0)
+	{
+		div = div * 10;
+		length++;
+	}
+	div = div / 10;
+	while (length > 0)
+	{
+		_putchar(((num / div) % 10) + '0');
+		div = div / 10;
+		length--;
+		id.count++;
+	}	
+}
+
+/**
+ * printf - prints int, char and strings
+ * @format: format specifier
+ * Return: 
+*/
+int _printf(const char *format, ...)
+{
+	id.i = 0, id.j = 0, id.count = 0;
+	if (format == NULL)
+		return (-1);
+	va_start(id.args, format);
+	while (format[id.i] != '\0')
+	{
+		if (format[id.i] == '%')
+		{
+			id.i++;
+			if (format[id.i] == 'c')
+				_printchar();
+			else if (format[id.i] == 's')
+				_printstr();
+			else if (format[id.i] == 'd' || format[id.i] == 'i')
+				_printint();
+			else if (format[id.i] == '%')
+				_putchar('%');
+			else
+			{
+				_putchar('%');
+				_putchar(format[id.i]);
+				id.count += 2;
+			}			
+		}
+		else
+		{
+			_putchar(format[id.i]);
+			id.count++;
+		}
+		id.i++;		
+	}
+	va_end(id.args);
+	return (id.count);	
+}
